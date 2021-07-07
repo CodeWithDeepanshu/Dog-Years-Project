8725709a54f5d4e1e77f99121f3921e2bb6ff556
CodeWithDeepanshu <85788386+CodeWithDeepanshu@users.noreply.github.com>
2021-07-08 00:53:55 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// Project by Deepanshu Jain
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  var age = Number(req.body.n1)/15;
+  res.send("Your age in Dog Years is : " + age + "dog years");
+});
+
+app.listen(port);
